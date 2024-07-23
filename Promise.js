71a71da573bd78815d29eb5f9aa3ee7026195726
yeyunwen <1571374338@qq.com>
2024-07-23 10:35:09 +0800

    feat: Promise构造器

---
+++ Promise.js
@@ -0,0 +1,34 @@
+const PENDING = "pending";
+const FULFILLED = "fulfilled";
+const REJECTED = "rejected";
+
+class MyPromise {
+  #state = PENDING;
+  #result = undefined;
+  constructor(executor) {
+    try {
+      executor(this.#_resolve, this.#_reject);
+    } catch (error) {
+      this.#_reject(error);
+    }
+  }
+
+  #_resolve = (data) => {
+    this.#changeState(FULFILLED, data);
+  };
+
+  #_reject = (reason) => {
+    this.#changeState(REJECTED, reason);
+  };
+
+  #changeState = (state, value) => {
+    console.log(state, value);
+    if (this.#state !== PENDING) return;
+    this.#state = state;
+    this.#result = value;
+  };
+}
+
+const p1 = new MyPromise((resolve, reject) => {
+  throw new Error("error");
+});
